2367c1c677e64c12c8fe9101b4996adb12328d61
camsmy <camille.monis@gmail.com>
2021-02-28 19:47:39 +0800

    home
    Things to do:
    - header to be fixed
    - design to change/alter
    - footer to add social media links
    - footer group credits

---
+++ footer.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row text-align-center">
+        <div class="col-xl-12 p-4 foot-column">
+            <img src="./assets/img/logo.png" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia fuga alias accusantium quam, tempore at earum veniam inventore natus impedit reiciendis quod, a quasi distinctio quidem. Temporibus ab ad illo.</p>
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -6,51 +6,86 @@ include 'head.php';
     <?php
 include 'navigation.php';
 ?>
-    <!-- <main>
-      <section class="glass">
-          <div class="container">
-            <div class="row text-align-center m-3">
-            <h1>Play your way and experience of gaming at home</h1>
-            <p>A Philippine game store aims to make it easy for customers to play their game at home.</p>
-            </div>
-          </div>
-      </section>
-    </main>
-    <div class="circle1"></div>
-    <div class="circle2"></div> -->
     <section>
         <div class="container">
             <div class="row">
                 <div class="col-xl-12 section-1 text-align-center position-relative head">
-                    <img src="./assets/img/imghead.jpg" alt="img-header" class="img-m">
+                    <img src="./assets/img/imghead.png" alt="img-header" class="img-m">
                     <img src="./assets/img/imgm.jpg" alt="img-header" class="img-d">
                     <div class="col-xl-7 col-xs-12 position-absolute head-txt">
                         <h2>Play your way and experience of gaming at home</h2>
                         <p class="p-2">A Philippine game rental store that aims to make it easy for
                             gamers to play their favorite games in the comfort of their home.
                         </p>
-                        <!-- <form id="search-form" class="form-inline" role="form" method="post"
-                            action="//www.google.com/search" target="_blank">
-                            <div class="input-group">
-                                <input type="text" class="form-control search-form" placeholder="Search">
-                                <span class="input-group-btn">
-                                    <button type="submit" class="btn btn-primary search-btn" data-target="#search-form"
-                                        name="q">
-                                        <i class="fa fa-search">
-                                        </i>
-                                    </button>
-                                </span>
-                            </div>
-                        </form> -->
-                        <form action="search-form" role="form" method="post" action="www.google.com" target="_blank">
-                        <input type="text" class="search-form" placeholder="Search" id="search" name="search">
-                        <button type="button" class="btn-search">Search</button>
+                        <form id="search-form-head" role="form" method="post" action="www.google.com" target="_blank">
+                            <input type="text" class="search-form" placeholder="Search" id="search" name="search">
+                            <button type="button" class="btn-search">Search</button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12 text-align-center m-2">
+                <h2>Discover New Gaming Experience</h2>
+            </div>
+        </div>
+    </div>
+
+    <section>
+        <div class="container">
+            <div class="row justify-content-center align-items-center column-reverse">
+                <div class="col-xl-2 px-2">
+                    <h3>Easy for Payment</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, hic sapiente officia aperiam sed
+                        in omnis eos ab aliquam modi libero, maxime porro vitae sint, ea labore inventore non sit?</p>
+                </div>
+                <div class="col-xl-2 m-5">
+                    <img src="./assets/img/payment.svg" alt="payment">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row justify-content-center align-items-center column">
+                <div class="col-xl-12 m-5 m-5">
+                    <img src="./assets/img/playing.svg" alt="playing">
+                </div>
+                <div class="col-xl-12 px-2">
+                    <h3>Play at the comfoprt of your home</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi exercitationem harum aut ratione odit fugiat officia autem vitae aperiam dolore doloremque, rem alias quia nemo ut maiores atque suscipit aspernatur?
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container">
+            <div class="row justify-content-center align-items-center column-reverse">
+                <div class="col-xl-2 px-2">
+                    <h3>More choices and games for you!</h3>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius, hic sapiente officia aperiam sed
+                        in omnis eos ab aliquam modi libero, maxime porro vitae sint, ea labore inventore non sit?</p>
+                </div>
+                <div class="col-xl-2 m-5">
+                    <img src="./assets/img/games.svg" alt="games">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="mt-5">
+    <?php
+include 'footer.php';
+?>
+    </footer>
 </body>
 
 </html>
