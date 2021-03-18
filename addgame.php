5f5b373afb71dca665741ad418549f41e372b9cc
Zaikishi <kyle99.garcia@gmail.com>
2021-03-18 20:51:20 +0800

    added addgame.css and updated addgame.php

--- addgame.php
+++ addgame.php
@@ -6,40 +6,27 @@ include 'admin-navigation.php';
 ?>
 <?php // OPENS THE DATABASE
 include 'opendb.php';
-?>
+?>  
 <div class="adminnavbar-main_content">
     <div class="login-container position-relative d-flex justify-content-center align-items-center">
-            <div class="signup-form2 position-relative d-flex justify-content-center align-items-center">
 
-                <form name="signup" method="post">
-                    <h3 class="login-title text-align-center">Add New Game</h3>
-                    <div class="login-input-container focus">
-                        <input type="text" name="sk" class="login-input" required>
-                        <label for="">SKU</label>
-                        <span>SKU</span>
-                    </div>
-                    <div class="login-input-container focus">
-                        <input type="text" name="gamen" class="login-input" required>
-                        <label for="">Game Name</label>
-                        <span>Game Name</span>
-                    </div>
-                    <div class="login-input-container focus">
-                        <input type="text" name="price" class="login-input" required>
-                        <label for="">Price</label>
-                        <span>Price</span>
-                    </div>
-                    <div class="login-input-container focus">
-                        <input type="text" name="available" class="login-input address" required>
-                        <label for="">Availability</label>
-                        <span>Availability</span>
-                    </div>
-                    <div class="login-input-container focus">
-                        <input type="file" name="image" id="file" required>
-                        <label for="">img</label>
-                        <span>img</span>
-                    </div>
-                    <div class="login-input-container focus">
-                        <select name ="Genre">
+                <form class="form-style-9">
+                  <h3 class="add-game text-align-center">Add New Game</h3>
+<ul>
+<li>
+    <input type="text" name="sk" class="field-style field-split align-left" placeholder="SKU" />
+    <input type="text" name="gname" class="field-style field-split align-right" placeholder="Game Name" />
+
+</li>
+<li>
+    <input type="text" name="price" class="field-style field-split align-left" placeholder="Price" />
+    <input type="text" name="available" class="field-style field-split align-right" placeholder="Availability" />
+</li>
+<li>
+<input type="file" name="image" class="field-style field-full align-none" placeholder="image" />
+</li>
+<li>
+  <div class = "field-style field-full align-none"> Genre: &nbsp &nbsp &nbsp  <select name ="genre">
                             <option>---Category---</option>
                             <option value ="Adventure">Adventure</option>
                             <option value ="Adventure">Horror</option>
@@ -48,14 +35,16 @@ include 'opendb.php';
                             <option value ="Adventure">Fighting</option>
                             <option value ="Adventure">Shooter</option>
                         </select>
-                    </div>
-                    <div class="login-input-container focus">
-                        <input type="email" name="gamedes" class="contact-input" required>
-                        <label for="">Game Description</label>
-                        <span>Game Description</span>
-                    </div>
-                    <input type="submit" name="Submit" value="Add Game" class="login-btn">
-                </form>
+                        </div>
+  </li>
+<li>
+<textarea name="gamedes" class="field-style" placeholder="Game Description"></textarea>
+</li>
+<li>
+<input type="submit" value="Add Game" />
+</li>
+</ul>
+</form>
             </div>
         </div>
     </div>
@@ -66,14 +55,33 @@ include 'opendb.php';
         $v1 = $_POST['sk'];
         $v2 = $_POST['gamen'];
         $v3 = $_POST['price'];
-        $v8 = $_POST['available'];
-        $v4 = $_POST['rented'];
-        $v5 = $_POST['reserved'];
-        $v6 = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
-        $v7 = $_POST['genre'];
-        $v9 = $_POST['gamedes'];
-        mysqli_query($DBConnect,"INSERT INTO gameinfo (sku,gname,price,avail,rent,reserve,img,genre,gdesc) VALUES ('$v1','$v2','$v3','$v4','$v5','$v6','$v7','$v8','$v9')");
+        $v4 = $_POST['available'];
+        $v5 = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));
+        $v6 = $_POST['genre'];
+        $v7 = $_POST['gamedes'];
+        mysqli_query($DBConnect,"INSERT INTO gameinfo (sku,gname,price,avail,img,genre,gdesc) VALUES ('$v1','$v2','$v3','$v4','$v5','$v6','$v7')");
     }?>
 </body>
-
+ <script>  
+ $(document).ready(function(){  
+      $('#insert').click(function(){  
+           var image_name = $('#image').val();  
+           if(image_name == '')  
+           {  
+                alert("Please Select Image");  
+                return false;  
+           }  
+           else  
+           {  
+                var extension = $('#image').val().split('.').pop().toLowerCase();  
+                if(jQuery.inArray(extension, ['gif','png','jpg','jpeg']) == -1)  
+                {  
+                     alert('Invalid Image File');  
+                     $('#image').val('');  
+                     return false;  
+                }  
+           }  
+      });  
+ });  
+ </script> 
 </html>
