6dd55b1da475e7eab047e7f71888ea54658203cf
camsmy <camille.monis@gmail.com>
2021-03-31 14:55:54 +0800

    contactform

--- contact.php
+++ contact.php
@@ -7,58 +7,81 @@ include 'head.php';
 if(!isset($_SESSION['user'])){ include 'navigation.php'; }
 else{ include 'customer-navigation.php'; }
 ?>
-
     <div class="contact-container position-relative d-flex justify-content-center align-items-center">
-    <span class="contact-big-circle"></span>  
-    <div class="row contact-form">
+        <span class="contact-big-circle"></span>
+        <div class="row contact-form">
             <div class="contact-info">
                 <h3 class="contact-title">Let's get in touch!</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe similique, hic nam necessitatibus
-                    excepturi doloremque aliquid quibusdam placeat, aliquam quod adipisci inventore. Ex dolore, saepe
-                    quasi alias ab corrupti cum?</p>
+                <p>You're not going to hit a ridiculously long phone menu when you call us. Your email isn't going to
+                    the inbox abyss, never to be seen or heard from again. At Choice Screening, we provide the
+                    exceptional service we'd want to experience ourselves!
+                </p>
                 <div class="contact-info-info">
                     <div class="contact-information">
-                        <p>insert address here</p>
+                        <i class="fas fa-map-marked-alt"></i>
+                        <p>Blk. 6 Lot 22 Oak St. Phase A Treelane III Bayan Luma IX 4103 Imus, Philippines</p>
                     </div>
                 </div>
                 <div class="contact-info-info">
                     <div class="contact-information">
-                        <p>email</p>
+                        <i class="fas fa-at"></i>
+                        <p>
+                            kej_novice831@yahoo.com
+                        </p>
                     </div>
                 </div>
                 <div class="contact-info-info">
                     <div class="contact-information">
-                        <p>contact num</p>
+                        <i class="fas fa-address-book"></i>
+                        <p>+63464547783</p>
                     </div>
                 </div>
                 <div class="contact-social-media">
                     <p>Connect with us:</p>
                     <div class="contact-social-icons">
-                        <a href="">
+                        <a href="https://www.facebook.com/kej.ps4.rental/">
                             <i class="fab fa-facebook-f"></i>
                         </a>
                     </div>
                 </div>
             </div>
-            
+
             <div class="contact-form2 position-relative">
                 <span class="contact-circle one position-absolute"></span>
                 <span class="contact-circle two position-absolute"></span>
-
-                <form action="contactform-code.php" method="post">
+                <form action="include/formvalidation.inc.php" method="post">
                     <h3 class="contact-title">Contact Us</h3>
+                    <?php
+                $fullurl= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+                if(strpos($fullurl,"form=empty")){
+                    echo "<p class='error text-align-center m-1'>You did not fill in all fields!</p>";
+                }
+                if(strpos($fullurl,"form=invalidemail")){
+                    echo "<p class='error text-align-center m-1'>Your email is invalid!</p>";
+                }
+                if(strpos($fullurl,"form=phone")){
+                    echo "<p class='error text-align-center m-1'>Your phone is invalid!</p>";
+                }
+                if(strpos($fullurl,"form=message")){
+                    echo "<p class='error text-align-center m-1'>Message shoud be below 70 characters</p>";
+                }
+                if(strpos($fullurl,"mailsend")){
+                    echo "<p class='error text-align-center m-1'>Message has been successfully sent!</p>";
+                }
+                ?>
                     <div class="contact-input-container focus">
-                        <input type="text" name="name" id="name" class="contact-input">
+                        <input type="text" name="name" id="name" class="contact-input <?=$invalid_class_name??""?>">
                         <label for="">Name</label>
                         <span>Name</span>
                     </div>
                     <div class="contact-input-container focus">
-                        <input type="email" name="email" id="email" class="contact-input">
+                        <input type="text" name="email" id="email" class="contact-input">
                         <label for="">Email</label>
                         <span>Email</span>
                     </div>
                     <div class="contact-input-container focus">
-                        <input type="tel" name="phone" id="phone" class="contact-input">
+                        <input type="text" name="phone" id="phone" class="contact-input">
                         <label for="">Telephone</label>
                         <span>Telephone</span>
                     </div>
@@ -67,14 +90,11 @@ else{ include 'customer-navigation.php'; }
                         <label for="">Message</label>
                         <span>Message</span>
                     </div>
-                    <input type="submit" value="Send Mail" class="contact-btn">
+                    <input type="submit" value="Send Mail" class="contact-btn" name="emailsubmit" onclick="sendEmail()">
                 </form>
-                
             </div>
         </div>
     </div>
-
-
 
     <div class="custom-shape-divider-top-1614623845 mt-5">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
