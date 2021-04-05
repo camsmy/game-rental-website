be12d6061517e443c07e8565faa544cce671a165
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2021-04-05 12:10:15 +0800

    fixed bugs

--- editgame.php
+++ editgame.php
@@ -3,11 +3,24 @@
 <?php
 include 'admin-navigation.php';
 include 'opendb.php';
+$query = mysqli_query($DBConnect,"SELECT sku,gname,price,rent,reserve from gameinfo WHERE sku='".$_SESSION['sku']."';") or die("ERROR");
+$game = mysqli_fetch_array($query);
+$isRented = $isReserved = false;
+if($game['rent']>0)
+    $isRented = true;
+if($game['reserve']>0)
+    $isReserved = true;
 if(isset($_POST['Edit'])){
     $sku = $_POST['sk'];
     $gname = mysqli_real_escape_string($DBConnect,$_POST['gname']);
     $price = $_POST['price'];
-    mysqli_query($DBConnect,"UPDATE gameinfo SET sku = '$sku',gname='$gname',price='$price' WHERE sku='$sku'") or die("ERROR UPDATING THE GAME!");
+    mysqli_query($DBConnect,"UPDATE gameinfo SET sku = '$sku',gname='$gname',price='$price' WHERE sku='".$_SESSION['sku']."';") or die("ERROR UPDATING THE GAME!");
+    if($isRented){
+        mysqli_query($DBConnect,"UPDATE rented SET rent_game = '$sku' WHERE rent_game ='".$_SESSION['sku']."';") or die("ERROR UPDATING THE rented!");
+    }
+    if($isReserved){
+        mysqli_query($DBConnect,"UPDATE reserved SET res_game = '$sku' WHERE res_game ='".$_SESSION['sku']."';") or die("ERROR UPDATING THE reserved!");
+    }
     unset($_SESSION['sku']);
     echo '<script>window.location="gamelist.php";</script>';
 }
@@ -18,8 +31,7 @@ if(isset($_POST['Edit'])){
             <h3 class="edit-game text-align-center">Edit Game</h3>
             <ul><li>
                 <?php
-                $query = mysqli_query($DBConnect,"SELECT sku,gname,price from gameinfo WHERE sku='".$_SESSION['sku']."';") or die("ERROR");
-                $game = mysqli_fetch_array($query);
+                
                 ?>
                     <p>SKU:</p><input type="text" name="sk" class="editgame-input" value="<?php echo $game['sku']?>"/>
                 </li>
