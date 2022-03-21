4c593044facb0d415574c5f839762889e27c4590
Dominic Narag <60565226+Dmncnrg@users.noreply.github.com>
2022-03-21 13:58:05 +0800

    updated database

--- opendb.php
+++ opendb.php
@@ -4,7 +4,7 @@ ERROR_REPORTING(E_NOTICE ^E_ALL);
 //$DBConnect = mysqli_connect("localhost","root","", "dbkejps") or die("Unable to connect".mysql_error());
 //mysqli_select_db($DBConnect,"game-rental-website");
 //Remote Database Connection
-// $DBConnect = mysqli_connect("remotemysql.com","dw8d6tfHPq","ZBWHxrPAy3","dw8d6tfHPq");
-$DBConnect = mysqli_connect("localhost","root","","kejpsdb");
+$DBConnect = mysqli_connect("remotemysql.com","ghdx0JPusb","RG4qFUd1wH","ghdx0JPusb");
+// $DBConnect = mysqli_connect("localhost","root","","kejpsdb");
 
 ?>
